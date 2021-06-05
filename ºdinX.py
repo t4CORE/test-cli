36ba31b3265bc975817510d823ee989c4c8ee90b
core <you@example.com>
2021-06-05 12:13:32 -0600

    1er commit

---
+++ ºdinX.py
@@ -0,0 +1,7 @@
+nput('introduzca la temperatura farenheith:')
+introduzca la temperatura farenheith:57
+>>> farh = float(ent)
+>>> cel = (farh - 32.0) * 5.0 / 9.0
+>>> print(cel)
+13.88888888888889
+
